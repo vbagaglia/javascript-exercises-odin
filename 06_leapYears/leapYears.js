26cdbc52f960ad93da224e07577fb4b40ee1116f
valen <valen.bagaglia@gmail.com>
2022-09-06 12:15:52 -0300

    added basic pseudo code in the comments

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -4,3 +4,10 @@ const leapYears = function() {
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// if divisible by 4 AND 400... true
+// if divisible by 100... false
+//  basically use if statement and && operators
+// Create a function that determines whether or not a given year is a leap year
+//eapYears(2000) // is a leap year: returns true
+//leapYears(1985) // is not a leap year: returns false
