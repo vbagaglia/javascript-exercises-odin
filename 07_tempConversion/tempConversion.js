e68959919067e466719ec818beef5ed07828b16d
valen <valen.bagaglia@gmail.com>
2022-09-09 11:48:11 -0300

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(farenheitNum) {
+  let celsiusConversion = (farenheitNum - 32) * (5/9);
+  let celsiusRounded = Math.round(celsiusConversion * 10) / 10;
 
+  return celsiusRounded
 };
 
-const ctof = function() {
+const ctof = function(celsiusNum) {
+  let farenheitConversion = (celsiusNum * (9/5) + 32);
+  let farenheitRounded = Math.round(farenheitConversion * 10) / 10;
 
+  return farenheitRounded;
 };
 
 // Do not edit below this line
