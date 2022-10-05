9386c70ad0bc1b1f2d3c2be898b88f8e049e5c24
valen <valen.bagaglia@gmail.com>
2022-10-05 10:35:35 -0300

    all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,20 +6,26 @@ const subtract = function(num1, num2) {
 	return num1 + num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((num1, num2) => num1 * num2);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 | num === 1) 
+    return 1;
+  
+  for (let i = num -1; i >= 1; i-- ) {
+     num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
