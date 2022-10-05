7bf5924e0589cbdf053d73c39698564a4b06d0f0
valen <valen.bagaglia@gmail.com>
2022-10-05 10:52:45 -0300

    fixed typo from my solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -19,7 +19,7 @@ const power = function(num1, num2) {
 };
 
 const factorial = function(num) {
-  if (num === 0 | num === 1) 
+  if (num === 0 || num === 1) 
     return 1;
   
   for (let i = num -1; i >= 1; i-- ) {
