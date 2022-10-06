ab75319b69a8ab709d2dc94f73d941e3d68de6ae
valen <valen.bagaglia@gmail.com>
2022-10-06 10:28:05 -0300

    test for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function (string) {
+const isPalindrome = function (str) {
+    let strRegex = str.replace(/[^a-z]/g, ""); // strips all punctuation with regex
 
+    let finalStripPunc = strRegex.replace(/ /g, ""); // removes multi word whitespace with regex
+
+    let finalString = finalStripPunc.toLowerCase;
+
+    let splitStr = finalString.split("");
+
+    let reverseArr = splitStr.reverse("");
+
+    let finalReverseStr = reverseArr.join("");
+
+    if(finalString === finalReverseStr) {
+        return true;
+    } else return false; 
 };
 
 // Do not edit below this line
