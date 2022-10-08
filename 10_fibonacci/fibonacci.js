f240d88a47d7dd0e17ce3d2a9cd7838b266fde2a
valen <valen.bagaglia@gmail.com>
2022-10-08 09:55:36 -0300

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+    let fibs = [1,1];
+    let fibSum = 0;
 
+    for(let i = 0; 0 < fib; i++) {
+        fibSum = fibs.at(-1) + fibs.at(-2); // pointing to the exact index to use
+        fibs.push(fibSum)
+    }
+    if (fib >= 0) {
+        return fibs[fib-1];
+    } else if (fib < o) {
+        return "No.";
+    }
 };
 
 // Do not edit below this line
