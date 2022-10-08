9d1edf8e0119f7e812d4c6823ed796e1d0eddb09
valen <valen.bagaglia@gmail.com>
2022-10-08 10:24:39 -0300

    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = (books) => {
+    let bookValues = books.map(book => book.title) // loop over book.titles
+    return bookValues;    
 };
 
 // Do not edit below this line
